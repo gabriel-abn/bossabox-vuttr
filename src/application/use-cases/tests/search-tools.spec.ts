d54bd21842ac1b466284e44cc5cf4614b1e7b98d
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-09-24 23:58:40 -0300

    test(SearchTool): :test_tube: added tests for search tool by title

--- src/application/use-cases/tests/search-tools.spec.ts
+++ src/application/use-cases/tests/search-tools.spec.ts
@@ -1,42 +1,84 @@
+import { SearchToolsUseCase } from "../search-tools-use-case";
 import { mockTool } from "./mocks/mock-tool";
 import { ToolRepositoryInMemory } from "./mocks/tool-repository-in-memory";
 
 const makeSut = () => {
   const repository = new ToolRepositoryInMemory();
-  const sut = new SearchToolByTagUseCase(repository);
+  const sut = new SearchToolsUseCase(repository);
 
   return { sut, repository };
 };
 
 describe("Use Case: Search Tool", () => {
-  it("should throw if tag does not exists", () => {
+  it("should throw if neither tag nor title is provided", () => {
     const { sut } = makeSut();
 
-    expect(
-      async () => await sut.execute({ tag: "invalid-tag" }),
-    ).rejects.toThrow("INVALID_TAG");
+    expect(async () => await sut.execute({})).rejects.toThrow("INVALID_PARAMS");
   });
 
-  it("should return a list of tools by tag", async () => {
-    const { sut, repository } = makeSut();
+  describe("Search by tag", () => {
+    it("should throw if tag does not exists", () => {
+      const { sut } = makeSut();
 
-    const tool = mockTool({ tags: ["tag2"] });
-
-    [
-      mockTool({ tags: ["tag1"] }),
-      mockTool(),
-      mockTool({ tags: ["tag1"] }),
-      mockTool(),
-      tool,
-    ].forEach((tool) => repository.add(tool));
-
-    await sut.execute({ tag: "tag1" }).then((tools) => {
-      expect(tools.lenght).toBe(2);
+      expect(
+        async () => await sut.execute({ tag: "invalid-tag" }),
+      ).rejects.toThrow("INVALID_TAG");
     });
 
-    await sut.execute({ tag: "tag2" }).then((tools) => {
-      expect(tools.lenght).toBe(1);
-      expect(tools[0].id).toBe(tool.id);
+    it("should return a list of tools by tag", async () => {
+      const { sut, repository } = makeSut();
+
+      const tool = mockTool({ tags: ["tag2"] });
+
+      [
+        mockTool({ tags: ["tag1"] }),
+        mockTool(),
+        mockTool({ tags: ["tag1"] }),
+        mockTool(),
+        tool,
+      ].forEach((tool) => repository.add(tool));
+
+      await sut.execute({ tag: "tag1" }).then((tools) => {
+        expect(tools.length).toBe(2);
+      });
+
+      await sut.execute({ tag: "tag2" }).then((tools) => {
+        expect(tools.length).toBe(1);
+        expect(tools[0].id).toBe(tool.id);
+      });
+    });
+  });
+
+  describe("Search by title", () => {
+    it("should throw if title does not exists", () => {
+      const { sut } = makeSut();
+
+      expect(
+        async () => await sut.execute({ title: "invalid-title" }),
+      ).rejects.toThrow("INVALID_TITLE");
+    });
+
+    it("should return a list of tools by title", async () => {
+      const { sut, repository } = makeSut();
+
+      const tool = mockTool({ title: "title2" });
+
+      [
+        mockTool({ title: "title1" }),
+        mockTool(),
+        mockTool({ title: "title1-addon" }),
+        mockTool(),
+        tool,
+      ].forEach((tool) => repository.add(tool));
+
+      await sut.execute({ title: "title1" }).then((tools) => {
+        expect(tools.length).toBe(2);
+      });
+
+      await sut.execute({ title: "title2" }).then((tools) => {
+        expect(tools.length).toBe(1);
+        expect(tools[0].id).toBe(tool.id);
+      });
     });
   });
 });
