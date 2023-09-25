0da00e65e063dfd092c7c268d540b531ceab855a
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-09-24 23:24:52 -0300

    refactor(SearchTool): :ambulance: changed use case name
    'SearchTool' use case will be able to seach for tools by name or tag.

--- src/domain/use-cases/index.ts
+++ src/domain/use-cases/index.ts
@@ -2,4 +2,4 @@ export * from "./add-tool";
 export * from "./delete-tool";
 export * from "./get-all-tools";
 export * from "./get-tool";
-export * from "./search-tools-by-tag";
+export * from "./search-tools";
---
+++ src/domain/use-cases/search-tools.ts
@@ -0,0 +1,16 @@
+import { UseCase } from "@domain/common";
+import { ToolProps } from "@domain/entities";
+
+export namespace SearchTools {
+  export type Params = Partial<{
+    tag: string;
+    title: string;
+  }>;
+
+  export type Result = ToolProps[];
+}
+
+export interface SearchTools
+  extends UseCase<SearchTools.Params, SearchTools.Result> {
+  execute(params: SearchTools.Params): Promise<SearchTools.Result>;
+}
