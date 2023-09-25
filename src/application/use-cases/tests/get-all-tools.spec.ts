503b4745857f1c855df915cc4a0adb6bfafb3a03
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-09-24 22:17:12 -0300

    test(GetAllTools): :test_tube: added failing tests

---
+++ src/application/use-cases/tests/get-all-tools.spec.ts
@@ -0,0 +1,28 @@
+import { ToolRepositoryInMemory } from "./mocks/tool-repository-in-memory";
+
+const makeSut = () => {
+  const repository = new ToolRepositoryInMemory();
+  const sut = new GetAllToolsUseCase(repository);
+
+  return { sut, repository };
+};
+
+describe("Use Case: Get All Tools", () => {
+  it("should return all tools", async () => {
+    const { sut, repository } = makeSut();
+
+    const tools = await sut.execute();
+
+    expect(tools).toEqual(repository.tools);
+  });
+
+  it("should return empty array if there is no tools", async () => {
+    const { sut, repository } = makeSut();
+
+    repository.tools = [];
+
+    const tools = await sut.execute();
+
+    expect(tools).toEqual([]);
+  });
+});
