e454f3f47c5de34b0787dd3d9f0899331b2484b7
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-02-18 18:24:36 -0300

    feat(Controller): :art: using controller as an abstract class
    All controllers will inherit from this abstract class, that implements all controllers' common behavior.

--- src/presentation/common/controller.ts
+++ src/presentation/common/controller.ts
@@ -1,5 +1,40 @@
-import { HttpResponse } from "./http";
+import { z } from "zod";
+import { HttpRequest, HttpResponse, badRequest, ok, serverError } from "./http";
 
-export interface Controller<T = any> {
-  handle: (request: T) => Promise<HttpResponse>;
+abstract class Controller<T = any> {
+  abstract run(request: T): Promise<any | HttpResponse>;
+  schema: z.ZodObject<any, any, any>;
+
+  constructor(schema: z.ZodObject<any, any, any>) {
+    if (!schema) {
+      this.schema = z.object({});
+    }
+
+    this.schema = schema;
+  }
+
+  async handle(request: HttpRequest<T>): Promise<HttpResponse> {
+    try {
+      const params = this.schema.safeParse(request.data);
+
+      if (params.success === false) {
+        return badRequest({
+          message: "Invalid request.",
+          errors: params.error.errors.map((error) => error.message),
+        });
+      }
+
+      const response = await this.run({ ...request.data });
+
+      if (typeof response.status != "undefined") {
+        return response;
+      }
+
+      return ok(response);
+    } catch (err) {
+      return serverError(err.message);
+    }
+  }
 }
+
+export default Controller;
