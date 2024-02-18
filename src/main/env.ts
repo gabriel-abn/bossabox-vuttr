b165b5ef798c857db7560ec05505f56349e39c30
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-02-18 18:54:42 -0300

    feat: added jwt secret env var to env object

--- src/main/env.ts
+++ src/main/env.ts
@@ -3,6 +3,7 @@ import * as z from "zod";
 const env = z.object({
   PORT: z.string().default("3000"),
   NODE_ENV: z.string().default("development"),
+  JWT_SECRET: z.string().default("secret"),
 });
 
 export default env.parse(process.env, {
