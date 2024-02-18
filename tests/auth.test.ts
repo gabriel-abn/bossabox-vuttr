1ec92bbb0ba407c74db2f11c5e8d1b330f862af6
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-02-18 18:04:04 -0300

    refactor(SignUp): :truck: refactored tests names

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -3,8 +3,8 @@ import app from "@main/server";
 import request from "supertest";
 
 describe("Auth: Sign up", () => {
-  describe("should verify credentials", () => {
-    it("should return 400 if no email is provided", async () => {
+  describe("should return 4XX if: ", () => {
+    it("if no email is provided", async () => {
       const res = await request(app).post("/api/auth/signup").send({
         email: "",
         password: "password",
@@ -13,7 +13,7 @@ describe("Auth: Sign up", () => {
       expect(res.status).toBe(400);
     });
 
-    it("should return 400 if no password is provided", async () => {
+    it("if no password is provided", async () => {
       const res = await request(app).post("/api/auth/signup").send({
         email: faker.internet.email(),
         password: "",
@@ -22,7 +22,7 @@ describe("Auth: Sign up", () => {
       expect(res.status).toBe(400);
     });
 
-    it("should return 400 if email is invalid", async () => {
+    it("if email is invalid", async () => {
       const res = await request(app).post("/api/auth/signup").send({
         email: "invalid-email",
         password: "password",
@@ -32,16 +32,29 @@ describe("Auth: Sign up", () => {
     });
 
     it.each(["gabriel", "gabriel123", "gabrielabc"])(
-      "should return 402 invalid password",
+      "invalid password [%s]",
       async (password: string) => {
         const res = await request(app).post("/api/auth/signup").send({
           email: faker.internet.email(),
           password,
         });
 
+        expect(res.body.error).toHaveProperty("errors", [
+          "Password must contain at least 8 characters, 1 uppercase letter, 1 lowercase letter and 1 number.",
+        ]);
         expect(res.status).toBe(400);
       },
     );
+  });
+
+  it("should return 201 if email and password are valid", async () => {
+    const res = await request(app).post("/api/auth/signup").send({
+      email: faker.internet.email(),
+      password: "Password123!",
+    });
+
+    expect(res.body).toHaveProperty("accessToken");
+    expect(res.status).toBe(201);
   });
 });
 
