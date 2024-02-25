7240fac99c05925e5bef368bdb19adb2a183aa77
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-02-24 22:42:30 -0300

    feat: :sparkles: added postgres url to env object

--- src/main/env.ts
+++ src/main/env.ts
@@ -4,6 +4,9 @@ const env = z.object({
   PORT: z.string().default("3000"),
   NODE_ENV: z.string().default("development"),
   JWT_SECRET: z.string().default("secret"),
+  POSTGRES_URL: z
+    .string()
+    .default("postgres://postgres:postgres@localhost:5432/postgres"),
 });
 
 export default env.parse(process.env, {
